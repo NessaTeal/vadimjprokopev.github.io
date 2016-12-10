f746d30a846040f6b9dcc18b5f82fb19b6245225
Kos94ok <kos94ok@gmail.com>
2016-12-10 12:11:19 +0200

    Karaoke!

--- kos/main.js
+++ kos/main.js
@@ -4,11 +4,11 @@ var f0 = 739.99;
 var g0 = 783.99;
 var a0 = 880;
 var b0 = 987.77;
-var c0 = 1046.50;
+var c1 = 1046.50;
 var d1 = d0 * 2;
 var e1 = e0 * 2;
 
-var notesInOrder = [d0, b0, a0, g0, d0,  d0, d0, d0, b0, a0, g0, e0, e0, c0, b0, a0, f0, d1, d1, c0, a0, b0, d0, b0, a0, g0, d0, d0, b0, a0, g0, e0,  e0, e0, c0, b0, a0, d1, d1, d1, d1, e1, d1, c0, a0, g0, b0, b0, b0, b0, b0, b0, b0, d1, g0, a0, b0, c0, c0, c0, c0, c0, b0, b0, b0, b0, b0, a0, a0, b0, a0, d1, b0, b0, b0, b0, b0, b0, b0, d1, g0, a0, b0, c0, c0, c0, c0, c0, b0, b0, b0, b0, d1, d1, c0, a0, g0];
+var notesInOrder = [d0, b0, a0, g0, d0,  d0, d0, d0, b0, a0, g0, e0, e0, c1, b0, a0, f0, d1, d1, c1, a0, b0, d0, b0, a0, g0, d0, d0, b0, a0, g0, e0,  e0, e0, c1, b0, a0, d1, d1, d1, d1, e1, d1, c1, a0, g0, b0, b0, b0, b0, b0, b0, b0, d1, g0, a0, b0, c1, c1, c1, c1, c1, b0, b0, b0, b0, b0, a0, a0, b0, a0, d1, b0, b0, b0, b0, b0, b0, b0, d1, g0, a0, b0, c1, c1, c1, c1, c1, b0, b0, b0, b0, d1, d1, c1, a0, g0];
 var lastsInOrder = [4,  4,  4,  4,  1.5, 8,  8,  4,  4,  4,  4,  1,  4,  4,  4,  4,  1,  4,  4,  4,  4,  1,  4,  4,  4,  4,  1,  4,  4,  4,  4,  1.5, 4,  4,  4,  4,  4,  4,  4,  3,  8,  4,  4,  4,  4,  1,  4,  4,  2,  4,  4,  2,  4,  4,  3,  8,  1,  4,  4,  3,  8,  4,  4,  4,  8,  8,  4,  4,  4,  4,  2,  2,  4,  4,  2,  4,  4,  2,  4,  4,  3,  8,  1,  4,  4,  3,  8,  4,  4,  4,  8,  8,  4,  4,  4,  4,  1];
 
 var audioCtx = new window.AudioContext;
@@ -36,8 +36,9 @@ function changeNote() {
 	var note = notesInOrder[i];
 	var last = lastsInOrder[i];
 	
+	karaoke_highlight(i);
 	i++;
-	if(i == notesInOrder.length) {
+	if (i == notesInOrder.length) {
 		i = 0;
 	}
 
@@ -49,4 +50,43 @@ function changeNote() {
 
 function makeQuiter() {
 	gainNode.gain.value = 0.040;
+}
+
+function karaoke_init() {
+	var output = "";
+	for (var i = 0; i < notesInOrder.length; i++) {
+		var style = "style='width: " + (1 / lastsInOrder[i]) * 150 + "'";
+		var div = "<div " + style + "class='karaokeFloater' id='karaoke_" + i + "'>";
+		var convert = "wut";
+
+		if (notesInOrder[i] == d0) convert = 'd0';
+		if (notesInOrder[i] == e0) convert = 'e0';
+		if (notesInOrder[i] == f0) convert = 'f0';
+		if (notesInOrder[i] == g0) convert = 'g0';
+		if (notesInOrder[i] == a0) convert = 'a0';
+		if (notesInOrder[i] == b0) convert = 'b0';
+		if (notesInOrder[i] == c1) convert = 'c1';
+		if (notesInOrder[i] == d1) convert = 'd1';
+		if (notesInOrder[i] == e1) convert = 'e1';
+
+		div += convert;
+		div += "</div>";
+		output += div;
+	}
+	document.getElementById('karaoke').innerHTML = output;
+	document.getElementById('karaoke_0').classList.add('karaokeRed');
+}
+
+function karaoke_highlight(i) {
+	var element;
+	if (i == 0) { element = document.getElementById("karaoke_" + (notesInOrder.length - 1)); }
+	else { element = document.getElementById("karaoke_" + (i - 1)); }
+	if (element != null) {
+		element.classList.remove('karaokeRed');
+	}
+
+	element = document.getElementById("karaoke_" + (i - 0));
+	if (element != null) {
+		element.classList.add('karaokeRed');
+	}
 }
