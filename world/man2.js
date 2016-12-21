e6cfc3c804c28d58d7cf39eddf93ed21607fc081
Vadim Prokopev <Ishvarloiva@gmail.com>
2016-12-21 10:52:11 +0100

    Github, dostal uzhe

---
+++ world/man2.js
@@ -0,0 +1,86 @@
+function man(x,y) {
+	this.x = x;
+	this.y = y;
+	this.path = undefined;
+	this.findPath = function findPath(target) {
+
+		var unvisited = $.extend(true, [], field);
+
+		unvisited[this.x][this.y].distance = 0;
+
+		var current = unvisited[this.x][this.y];
+
+		while(current.x != target.x || current.y != target.y) {
+
+			if(current.y != FIELD_HEIGHT / CELL_SIZE - 1
+				&& (unvisited[current.x][current.y + 1].distance === undefined
+				|| unvisited[current.x][current.y + 1].distance > current.distance + unvisited[current.x][current.y + 1].cost)) {
+
+				unvisited[current.x][current.y + 1].distance = current.distance + unvisited[current.x][current.y + 1].cost;
+				unvisited[current.x][current.y + 1].previous = current;
+			};
+
+			if(current.y != 0
+				&& (unvisited[current.x][current.y - 1].distance === undefined
+				|| unvisited[current.x][current.y - 1].distance > current.distance + unvisited[current.x][current.y - 1].cost)) {
+
+				unvisited[current.x][current.y - 1].distance = current.distance + unvisited[current.x][current.y - 1].cost;
+				unvisited[current.x][current.y - 1].previous = current;
+			};
+
+			if(current.x != FIELD_LENGTH / CELL_SIZE - 1
+				&& (unvisited[current.x + 1][current.y].distance === undefined
+				|| unvisited[current.x + 1][current.y].distance > current.distance + unvisited[current.x + 1][current.y].cost)) {
+
+				unvisited[current.x + 1][current.y].distance = current.distance + unvisited[current.x + 1][current.y].cost;
+				unvisited[current.x + 1][current.y].previous = current;
+			};
+
+			if(current.x != 0
+				&& (unvisited[current.x - 1][current.y].distance === undefined
+				|| unvisited[current.x - 1][current.y].distance > current.distance + unvisited[current.x - 1][current.y].cost)) {
+
+				unvisited[current.x - 1][current.y].distance = current.distance + unvisited[current.x - 1	][current.y].cost;
+				unvisited[current.x - 1][current.y].previous = current;
+			};
+
+			current.visited = true;
+
+			var newCur;
+			var smallestDist = 20000;
+
+			for(var i = 0; i < FIELD_LENGTH / CELL_SIZE; i++) {
+				for(var j = 0; j < FIELD_HEIGHT / CELL_SIZE; j++) {
+					if(unvisited[i][j].visited != true || unvisited[i][j].visited === undefined) {
+						if(unvisited[i][j].distance < smallestDist) {
+							newCur = unvisited[i][j];
+							smallestDist = unvisited[i][j].distance;
+						}
+					}
+				}
+			}
+
+			current = newCur;
+		}
+
+		var path = [current];
+
+		while(current.previous !== undefined) {
+			path.unshift(current.previous);
+			current = current.previous;
+		}
+
+		path.splice(0,1);
+
+		this.path = path;
+	}
+
+	this.move = function() {
+		if(this.path.length != 0) {
+			this.x = this.path[0].x;
+			this.y = this.path[0].y;
+
+			this.path.splice(0,1);
+		}
+	}
+}
